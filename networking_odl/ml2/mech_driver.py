be3e9afb93abb2d6898fb71ea595a067e97a05ca
Isaku Yamahata <isaku.yamahata@intel.com>
2015-08-27 12:29:29 -0700

    odl client: gracefully ignore 404 when deleting
    When neutron and odl are out of sync, it's possible for neutron to
    send delete request to ODL on the resource which ODL doesn't know.
    In this case not found error code, 404, can be ignored gracefully
    instead of returning an error to user.
    This patch depends on ODL neutron northbound fix so that
    it can return 404 instead of internal server error.
    
    Change-Id: I2c7e699c227f1fa3251861a2698410666f767701
    Closes-Bug: #1489593

--- networking_odl/ml2/mech_driver.py
+++ networking_odl/ml2/mech_driver.py
@@ -197,8 +197,8 @@ class OpenDaylightDriver(object):
         try:
             obj_id = context.current['id']
             if operation == odl_const.ODL_DELETE:
-                self.client.sendjson('delete', object_type_url + '/' + obj_id,
-                                     None)
+                self.out_of_sync |= not self.client.try_delete(
+                    object_type_url + '/' + obj_id)
             else:
                 if operation == odl_const.ODL_CREATE:
                     urlpath = object_type_url
@@ -225,8 +225,8 @@ class OpenDaylightDriver(object):
                            resource_dict):
         try:
             if operation == odl_const.ODL_DELETE:
-                self.client.sendjson('delete', object_type + '/' + res_id,
-                                     None)
+                self.out_of_sync |= not self.client.try_delete(
+                    object_type + '/' + res_id)
             else:
                 if operation == odl_const.ODL_CREATE:
                     urlpath = object_type
